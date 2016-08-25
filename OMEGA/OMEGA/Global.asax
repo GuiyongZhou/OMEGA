474d14c354c532580c473ece5feba99ccf9a2bd0
周贵勇 <516958642@qq.com>
2016-08-25 18:48:11 +0800

    新建OMEGA

---
+++ OMEGA/OMEGA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OMEGA.MvcApplication" Language="C#" %>
---
+++ OMEGA/OMEGA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OMEGA.Startup))]
+namespace OMEGA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
